8c1c1c25d08812dcdbb6546693420f4545ec8e5c
Mike Holt <CutFlame@gmail.com>
2020-11-17 22:05:15 -0700

    Add app icon and assembly info

--- MCDSaveEdit/Properties/AssemblyInfo.cs
+++ MCDSaveEdit/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using MCDSaveEdit;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -8,7 +9,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MCDSaveEdit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Save game editor for Minecraft Dungeons")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MCDSaveEdit")]
@@ -51,5 +52,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion(Constants.CURRENT_RELEASE_TAG_NAME)]
