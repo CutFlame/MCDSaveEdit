a93ff728d99afe2238e91713a2216983711060fe
Mike Holt <CutFlame@gmail.com>
2021-03-24 20:42:47 -0600

    Version bump to 1.3.2

--- MCDSaveEdit/Data/Constants.cs
+++ MCDSaveEdit/Data/Constants.cs
@@ -7,7 +7,7 @@ namespace MCDSaveEdit
 {
     public static partial class Constants
     {
-        public const string CURRENT_VERSION_NUMBER = "1.3.1";
+        public const string CURRENT_VERSION_NUMBER = "1.3.2";
         public const string LATEST_RELEASE_GITHUB_URL = "https://github.com/CutFlame/MCDSaveEdit/releases/latest";
         public const string LATEST_RELEASE_MOD_URL_FORMAT = "https://www.nexusmods.com/minecraftdungeons/mods/{0}?tab=files";
 
